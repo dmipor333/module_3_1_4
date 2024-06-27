c2253d8409ec3b26c0531508430901df6f85b750
dmipor333 <dmipor333@ya.ru>
2024-06-27 20:52:46 +0300

    Add files via upload

---
+++ module_3_1_4,py.py
@@ -0,0 +1,17 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+count_calls()
+def string_info(string):
+    string1 = {len(string), string.upper(), string.lower()}
+    print(string1)
+string_info('Copibara')
+count_calls()
+def is_contains(string, list_to_search):
+    string.lower()
+    list_to_search.lower()
+    print(string in list_to_search)
+    is_contains('Urban', *['urBAN', 'BaNaN', 'ban'])
+count_calls()
+print(calls)
